f61cb7f2c967f2e315a3a99d28b0978c67afab81
hackerscoh <159046873+hackerscoh@users.noreply.github.com>
2024-08-23 17:25:03 +0000

    qwerty

---
+++ bot.py
@@ -0,0 +1,45 @@
+from pyrogram import Client, filters
+import requests
+import random
+
+# api_id = 25593180 #--Add your Api Id here
+# api_hash = 'b58bd82141f66e627ba6c4eb480a3dd3' #--Enter Api Hash Here
+
+# token = '6916100902:AAFpi3Y6GFv4vpaaRa9H9SEzv4-wxmlk6jA' #--Enter Bot Token Here.
+
+api_id = '25593180'
+api_hash = 'b58bd82141f66e627ba6c4eb480a3dd3'
+# bot_token = '6916100902:AAE1oZS0MmN3FhqjLpi80PVTJVMF_CeTk1g'
+app = Client("user", api_id, api_hash)
+from pyrogram.raw import functions, types
+from pyrogram import  Client, filters
+import asyncio
+import time
+import pytz
+from datetime import datetime,timedelta
+import random
+async def change_name():
+    while True:
+        
+        
+        try:
+            current_time = time.strftime("%H:%M:%S")
+            utc_time = datetime.utcnow()
+       
+            ist = pytz.timezone('Asia/Kolkata')
+        
+            ti=(utc_time.astimezone(ist)+ timedelta(minutes=1)).strftime("%I:%M %p")
+          
+            await app.update_profile(last_name=ti)
+           
+        except Exception as e:
+            await app.send_message(-10020965765, e)
+        await asyncio.sleep(60)  # Sleep for 60 seconds (1 minute)
+
+
+
+async def main():
+    async with app:
+        await change_name()
+
+app.run(main())
