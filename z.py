b8e803d82286f298eb103d8ef632798a822a1e65
mahesh <159046873+hackerscoh1@users.noreply.github.com>
2025-03-02 15:57:16 +0000

    k

--- z.py
+++ z.py
@@ -2,5 +2,5 @@ from bing_image_downloader import downloader
 
 query_string = str(input('Enter Your Query: '))
 
-raj = downloader.download(query_string, limit=100,  output_dir='dataset', adult_filter_off=True, force_replace=False, timeout=60, verbose=True)
+raj = downloader.download(query_string, limit=1,  output_dir='dataset', adult_filter_off=True, force_replace=False, timeout=60, verbose=True)
 print(raj)
